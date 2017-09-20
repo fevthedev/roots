fe08a0ad71714913b4cdc6e62083b526d3bda878
Jonathan MacKenzie <jmail97@gmail.com>
2017-09-20 11:26:51 -0300

    comment tools.js in routes.js

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 //routes.js
 
 var as    = require('async'); // 'async' is a reserved word now
-var tools = require('./tools.js');
+//var tools = require('./tools.js');
 
 module.exports = function(app, db)
 {
